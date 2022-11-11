5c4dff93b3a0ba801e8c8c711918da5a990b80fd
DeepikaAlapati <65488710+DeepikaAlapati@users.noreply.github.com>
2022-11-11 13:16:05 -0600

    Update Balloon.js

--- controllers/Balloon.js
+++ controllers/Balloon.js
@@ -12,15 +12,27 @@ exports.Balloon_list = async function(req, res) {
     }   
 }; 
 
+exports.Balloon_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Balloon.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+
+
 // for a specific Balloon. 
-exports.Balloon_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Balloon detail: ' + req.params.id); 
-}; 
+//exports.Balloon_detail = function(req, res) { 
+  //  res.send('NOT IMPLEMENTED: Balloon detail: ' + req.params.id); 
+//}; 
 
 // Handle Balloon create on POST. 
-exports.Balloon_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Balloon create POST'); 
-}; 
+//exports.Balloon_create_post = function(req, res) { 
+  //  res.send('NOT IMPLEMENTED: Balloon create POST'); 
+//}; 
 
 // Handle Balloon delete form on DELETE. 
 exports.Balloon_delete = function(req, res) { 
@@ -28,8 +40,25 @@ exports.Balloon_delete = function(req, res) {
 }; 
 
 // Handle Balloon update form on PUT. 
-exports.Balloon_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Balloon update PUT' + req.params.id); 
+exports.Balloon_update_put = async function(req, res) { 
+   // res.send('NOT IMPLEMENTED: Balloon update PUT' + req.params.id); 
+   console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Balloon.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.balloon_color)
+    toUpdate.balloon_color = req.body.balloon_color;
+    if(req.body.balloon_type) toUpdate.balloon_type = req.body.balloon_type;
+    if(req.body.balloon_size) toUpdate.balloon_size = req.body.balloon_size;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
 }; 
 // VIEWS 
 // Handle a show all view 
