ac1a7f28b48dd171328394611d5b5e98c5eaee77
DeepikaAlapati <65488710+DeepikaAlapati@users.noreply.github.com>
2022-11-10 22:29:09 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,30 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+require('dotenv').config(); 
+const connectionString =  process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  
+{useNewUrlParser: true, 
+useUnifiedTopology: true}); 
+
+//Get the default connection 
+var db = mongoose.connection; 
+
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+  console.log("Connection to DB succeeded")
+});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var balloonRouter = require('./routes/balloon');
 var gridbuildRouter = require('./routes/gridbuild');
 var selectorRouter = require('./routes/selector');
+const Balloon = require('./models/Balloon');
+var resourceRouter = require('./routes/resource');
 
 
 
@@ -29,6 +48,7 @@ app.use('/users', usersRouter);
 app.use('/balloon',balloonRouter);
 app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selectorRouter);
+app.use('/resource',resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,5 +65,45 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+async function recreateDB(){ 
+  // Delete everything 
+  await Balloon.deleteMany(); 
+  let instance1 = new 
+  Balloon({
+    balloon_color: "black",
+    balloon_type:"large",
+    balloon_size : 7
+  }); 
+  let instance2 = new 
+  Balloon({
+    balloon_color: "blue",
+    balloon_type:"small",
+    balloon_size : 5
+  }); 
+  let instance3 = new 
+  Balloon({
+    balloon_color: "purple",
+    balloon_type:"medium",
+    balloon_size : 10,
+  }); 
+  instance1.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("First Balloon object saved") 
+  });
+  instance2.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("Second Balloon object saved") 
+  });
+  instance3.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("Third Balloon object saved") 
+  }); 
+} 
+
+let reseed = true; 
+if (reseed) { 
+  recreateDB();
+} 
 
 module.exports = app;
