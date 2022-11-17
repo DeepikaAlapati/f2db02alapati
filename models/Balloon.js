d119d610f1423b75bb07022c65fd4c8468056424
DeepikaAlapati <65488710+DeepikaAlapati@users.noreply.github.com>
2022-11-16 23:34:47 -0600

    Update Balloon.js

--- models/Balloon.js
+++ models/Balloon.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose") 
 const balloonSchema = mongoose.Schema({ 
+    balloonid: Number,
     balloon_color: String,
     balloon_type:String,
     balloon_size : Number,
