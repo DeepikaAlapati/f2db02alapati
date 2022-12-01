b816fd34c542af1cc9876d76560744b585d4d5d1
DeepikaAlapati <65488710+DeepikaAlapati@users.noreply.github.com>
2022-11-30 18:14:06 -0600

    Update balloon.js

--- routes/balloon.js
+++ routes/balloon.js
@@ -1,11 +1,24 @@
 var express = require('express');
+var Balloon_controller = require('../controllers/Balloon');
 var router = express.Router();
 
-const Balloon_controllers = require('../controllers/Balloon');
-/* GET home page. */
-router.get('/',Balloon_controllers.Balloon_view_all_Page);
-
-
-
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+router.get('/', Balloon_controller.Balloon_view_all_Page ); 
+// GET request for one Balloon. 
+router.get('/Balloon/:id', Balloon_controller.Balloon_detail);
+/* GET detail Balloon page */ 
+router.get('/detail', Balloon_controller.Balloon_view_one_Page); 
+/* GET create Balloon page */ 
+router.get('/create',secured, Balloon_controller.Balloon_create_Page); 
+/* GET create update page */ 
+router.get('/update',secured, Balloon_controller.Balloon_update_Page);
+/* GET delete Balloon page */ 
+router.get('/delete',secured, Balloon_controller.Balloon_delete_Page);
 module.exports = router;
- 
