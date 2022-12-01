aba5b95a19b1ad78fc24704ee5e954552694fb8e
DeepikaAlapati <65488710+DeepikaAlapati@users.noreply.github.com>
2022-11-30 18:13:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var mongoose = require('mongoose');
-var mongodb = require('mongodb');
+//var mongoose = require('mongoose');
+//var mongodb = require('mongodb');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//var indexRouter = require('./routes/index');
 var passport = require('passport'); 
 var LocalStrategy = require('passport-local').Strategy;
 
 
-passport.use(new LocalStrategy( 
+passport.use(new LocalStrategy)(
   function(username, password, done) { 
     Account.findOne({ username: username }, function (err, user) { 
       if (err) { return done(err); } 
@@ -20,12 +21,9 @@ passport.use(new LocalStrategy(
         return done(null, false, { message: 'Incorrect password.' }); 
       } 
       return done(null, user); 
-    }); 
-  }
-  
-var Balloon = require('./models/Balloon');
-
-
+  });
+}
+);
 
 
 
@@ -33,8 +31,10 @@ require('dotenv').config();
 const connectionString =  process.env.MONGO_CON 
 mongoose = require('mongoose'); 
 mongoose.connect(connectionString,  
-{useNewUrlParser: true, 
-useUnifiedTopology: true}); 
+{
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+}); 
 
 //Get the default connection 
 var db = mongoose.connection; 
@@ -45,14 +45,14 @@ db.once("open", function(){
   console.log("Connection to DB succeeded")
 });
 
+
+var resourceRouter = require('./routes/resource');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var balloonRouter = require('./routes/balloon');
 var gridbuildRouter = require('./routes/gridbuild');
 var selectorRouter = require('./routes/selector');
-var resourceRouter = require('./routes/resource');
-
-
+const balloon = require('./models/Balloon');
 var app = express();
 
 // view engine setup
@@ -63,7 +63,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+}));
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 app.use(express.static(path.join(__dirname, 'public')));
+
+var Account =require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -72,38 +85,6 @@ app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource',resourceRouter);
 
-
-app.use(require('express-session')({ 
-  secret: 'keyboard cat', 
-  resave: false, 
-  saveUninitialized: false 
-})); 
-app.use(passport.initialize()); 
-app.use(passport.session()); 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-var Account =require('./models/account')); 
- 
-passport.use(new LocalStrategy(Account.authenticate())); 
-passport.serializeUser(Account.serializeUser()); 
-passport.deserializeUser(Account.deserializeUser()); 
 
 async function recreateDB(){ 
   // Delete everything 
@@ -144,5 +125,33 @@ let reseed = true;
 if (reseed) { 
   recreateDB();
 } 
+
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// for a specific hat. 
+exports.balloon_detail = async function(req, res) { 
+  console.log("detail"  + req.params.id) 
+  try { 
+      result = await balloon.findById( req.params.id) 
+      res.send(result) 
+  } catch (error) { 
+      res.status(500) 
+      res.send(`{"error": document for id ${req.params.id} not found`); 
+  } 
+}; 
 
 module.exports = app;
